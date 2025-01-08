ef49b4f182d5a3591d9963c77f261172c09064b5
benwei <benwei@WEIDYPC>
2025-01-08 15:43:48 +0800

    增加Lable

--- CMToolKitTest/Form1.Designer.cs
+++ CMToolKitTest/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace CMToolKitTest
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -60,11 +61,21 @@ namespace CMToolKitTest
             this.label1.TabIndex = 2;
             this.label1.Text = "这是一个CM动态库的示例";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(461, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -81,6 +92,7 @@ namespace CMToolKitTest
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
